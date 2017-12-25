abc2c6c974fe89ec953a8e1206110c2308fe4d6f
Brookeryang <yonghong1355857303@163.com>
2017-12-25 16:02:35 +0800

    添加Test类

---
+++ dssignsupportlibrary/src/main/java/soft/boke/com/dssignsupportlibrary/Test.java
@@ -0,0 +1,30 @@
+package soft.boke.com.dssignsupportlibrary;
+
+/**
+ * <pre>
+ *     author : yyh
+ *     time :  2017/12/25 16:00
+ *     version: 1.0
+ *     desc   : 描述XXXX
+ * </pre>
+ */
+public class Test {
+    private String name;
+    private String pawass;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPawass() {
+        return pawass;
+    }
+
+    public void setPawass(String pawass) {
+        this.pawass = pawass;
+    }
+}
