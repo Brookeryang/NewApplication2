507d683cd05cd2c55854859136c4d6a11fcbf855
Brookeryang <yonghong1355857303@163.com>
2017-12-25 16:13:49 +0800

    添加Test类

---
+++ dssignsupportlibrary/src/main/java/soft/boke/com/dssignsupportlibrary/AAAAAA.java
@@ -0,0 +1,12 @@
+package soft.boke.com.dssignsupportlibrary;
+
+/**
+ * <pre>
+ *     author : yyh
+ *     time :  2017/12/25 16:12
+ *     version: 1.0
+ *     desc   : 描述XXXX
+ * </pre>
+ */
+public class AAAAAA {
+}
